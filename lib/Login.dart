09cbae755164cc60c9f4f3b3c0e90f64abe7c617
Rafael <a22001810@alunos.ulht.pt>
2023-03-16 10:41:37 +0000

    Initial commit

---
+++ lib/Login.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _submit() async {
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    //final authService = AuthService();
+    //try {
+      //final token = await authService.login(email, password);
+      // Login bem-sucedido, salvar o token e navegar para a próxima tela
+    //} catch (e) {
+      // Login falhou, exibir mensagem de erro
+    //}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'E-mail é obrigatório';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Senha é obrigatória';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _submit();
+                  }
+                },
+                child: Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
