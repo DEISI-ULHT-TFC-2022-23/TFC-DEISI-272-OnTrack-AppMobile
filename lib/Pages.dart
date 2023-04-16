1811532f6e4883d86e4e49550284c50b31a169bb
RafaelPaulo a22001810 <a22001810@alunos.ulht.pt>
2023-04-16 18:40:42 +0100

    Correção do drawer

--- lib/Pages.dart
+++ lib/Pages.dart
@@ -60,6 +60,7 @@ class _PagesState extends State<Pages> {
       ),
       drawer: Drawer(
         child: ListView(
+          padding: const EdgeInsets.all(0.0),
           children: <Widget>[
             UserAccountsDrawerHeader(
               decoration: BoxDecoration(
